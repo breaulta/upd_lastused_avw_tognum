2f452555a9816c59f0bde1c457a96a7b3ea4dd9b
Aaron Breault <aaronbreault@gmail.com>
2020-03-06 16:41:03 -0800

    comments/punctuation

--- edit_gnumeric_ss.pl
+++ edit_gnumeric_ss.pl
@@ -5,16 +5,17 @@ use warnings;
 
 #hardcoded for now; will change later
 my $gnumeric_ss = "sample_spreadsheet.gnumeric";
+
 #Use regex so code is applicable to any filename.
 $gnumeric_ss =~ /(.+)\.gnumeric$/;
 my $ss = $1;
 my $gz_ss = "$1.gz";
 
-#Will need to regex match the filename here for the .gnumeric to .gz swap
+#Convert file to type that is useable by perl.
 rename $gnumeric_ss, $gz_ss;
-system("gunzip", $gz_ss) == 0 or die "system call failed: $?";
+system("gunzip", $gz_ss) == 0 or die "System call failed: $?";
 system("cp", $ss, "copyof_sample_spreadsheet");
 
 #cleanup
-system("gzip", $ss) == 0 or die "system call failed: $?";
-system("mv", $gz_ss, $gnumeric_ss) == 0 or die "system call failed: $?";
+system("gzip", $ss) == 0 or die "System call failed: $?";
+system("mv", $gz_ss, $gnumeric_ss) == 0 or die "System call failed: $?";
