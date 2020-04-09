5597a60c3d2855a126e2c4be5a401534ee79b48c
Len <you@example.com>
2020-04-09 13:23:59 -0700

    Fixed a few bugs and added comments with Aaron

--- update_spreadsheet.pl
+++ update_spreadsheet.pl
@@ -35,11 +35,11 @@ my $email_to_row_hashref = get_emails();
 #Login to each account, populate account_data hash, and write the data to a new file.
 foreach my $email (keys %$email_to_row_hashref){
 	my $data_hashref = get_account_data($email, $password, 1);
-#$editor->writecell($ss_field_to_letter{'expire'} . $$email_to_row_hashref{$email}, $$data_hashref{'plan_expire_date'});
+	$editor->writecell($ss_field_to_letter{'expire'} . $$email_to_row_hashref{$email}, $$data_hashref{'plan_expire_date'});
 	$editor->writecell($ss_field_to_letter{'call_date'} . $$email_to_row_hashref{$email}, $$data_hashref{'last_call_date'});
-#$editor->writecell($ss_field_to_letter{'text_date'} . $$email_to_row_hashref{$email}, $$data_hashref{'last_outgoing_text_date'});
-#$editor->writecell($ss_field_to_letter{'data_date'} . $$email_to_row_hashref{$email}, $$data_hashref{'last_data_date'});
-#$editor->writecell($ss_field_to_letter{'rem_date'} . $$email_to_row_hashref{$email}, $$data_hashref{'remaining_data'});
+	$editor->writecell($ss_field_to_letter{'text_date'} . $$email_to_row_hashref{$email}, $$data_hashref{'last_outgoing_text_date'});
+	$editor->writecell($ss_field_to_letter{'data_date'} . $$email_to_row_hashref{$email}, $$data_hashref{'last_data_date'});
+	$editor->writecell($ss_field_to_letter{'rem_data'} . $$email_to_row_hashref{$email}, $$data_hashref{'remaining_data'});
 	$editor->savefile('test.gnumeric');
 }
 
@@ -135,21 +135,21 @@ sub get_account_data {
 		$phone_fields{'last_data_date'} = $1 if $html_line =~ m/date..(\d{2}.\d{2}.\d{4})/ and not defined $phone_fields{'last_data_date'};
 	}
 
-	#Fetch account information for account.
+	#Fetch account information HTML for account.
 	$mech->get($base_url . "/my-account/account-information?a=" . $account_number);
 	sleep(1);
-	my $account_info = $mech->content( decoded_by_headers => 1 );
-	unless ($account_info =~ m/Airtime Exp Date/){
+	my $account_info_html = $mech->content( decoded_by_headers => 1 );
+	unless ($account_info_html =~ m/Airtime Exp Date/){
 		die "Account information page failed to load for account $username\n" if $login_attempt_num > 2;
 		$login_attempt_num++;
 		return get_account_data($username, $account_pass, $login_attempt_num);
 	}
-	$phone_fields{'plan_type'} = $1 if $account_info =~ m/"PlanName":"([^"]+)"/;
-	$phone_fields{'account_balance'} = $1 if $account_info =~ m/"AccountBalance":"(\$\d{1,2}\.\d{2})"/;
+	$phone_fields{'plan_type'} = $1 if $account_info_html =~ m/"PlanName":"([^"]+)"/;
+	$phone_fields{'account_balance'} = $1 if $account_info_html =~ m/"AccountBalance":"(\$\d{1,2}\.\d{2})"/;
 	#Formatted as MM/DD/YYYY.
-	$phone_fields{'plan_expire_date'} = "$2/$3/$1" if $account_info =~ m/"AirtimeExpDate":"(\d{4})-(\d{2})-(\d{2})[^"]+"/;
+	$phone_fields{'plan_expire_date'} = "$2/$3/$1" if $account_info_html =~ m/"AirtimeExpDate":"(\d{4})-(\d{2})-(\d{2})[^"]+"/;
 	#Grab kb data, divide to turn to gb, round to 2 decimals.
-	$phone_fields{'remaining_data'} = sprintf("%.2f GB", $1/1e6) if $account_info =~ m/"Data":"(\d+)"/;
+	$phone_fields{'remaining_data'} = sprintf("%.2f GB", $1/1048576) if $account_info_html =~ m/"Data":"(\d+)"/;
 	#Ensure that no fields have been left undefined before returning hashref.
 	foreach my $field (keys %phone_fields){
 		unless (defined $phone_fields{$field}){
