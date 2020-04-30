4890b96de381ec6f57a1f0abaa4f83a183fceab3
Aaron Breault <aaronbreault@gmail.com>
2020-04-30 15:38:24 -0700

    Looks like final changes to writecell are working.

--- Editor.pm
+++ Editor.pm
@@ -107,62 +107,41 @@ sub writecell {
 	my $cell = shift;
 	my $data = shift;
 
-	# Len suggests to check for undef data => kill program.
+	die "writecell call received undefined input!"
+		unless defined $data;
 	if ($data eq "") {
 		print "No data to write; deleting cell $cell\n";
 	}
-# write a sub for converting cell names to gnumeric cell format.
 	#Split letter from number
-	$cell =~ /(\w)(\d+)/;
+	die "readcell call not executed properly: Failed to split cell index letter from number."
+		unless $cell =~ /(\w)(\d+)/;
 	my $column = uc $1;	#Set everything to uppercase.
 	my $row = $2;
-	#print "Reading user input- Col:$column, Row:$row\n";
-
 	#Dereference letter to number using %cell_to_gnu_map, rows start at 0 instead of 1.
 	my $gnu_column = $cell_to_gnu_map{$column};
 	$row--;
-	#print "Type used by gnumeric- Col:$column, Row:$row\n";
-
-	#Find line that corresponds to cell
-	#	open file associated with this object
-	#open( my $fh, "<", $temp_file) or die "Can't open $temp_file: $!";
-	#	from: https://stackoverflow.com/questions/2278527/how-do-i-replace-lines-in-the-middle-of-a-file-with-perl
-	#	Since perl doesn't provide random access to lines, we must create a new file.
-#	open( my $fhout, ">", "$temp_file.out") or die "Can't open $temp_file.out: $!";
-	#	
-#	while( <$fh> ){
+	my $endcell = '</gnm:Cell>';
 	#Keep track of the current index of the array.
 	#foreach (@temp_file){
 	for( my $i = 0; $i < scalar @temp_file; $i++){
 		my $line = $temp_file[$i];
-		if ( $line =~ /\<gnm\:Cell Row\=\"$row\" Col\=\"$gnu_column\" ValueType\=\"\d+\"\ ValueFormat\=\"m\/d\/yyyy\"\>(.*)\<\/gnm\:Cell\>/ ){
+		if( $line =~ /Row..$row. Col..$gnu_column.+\"m\/d\/yyyy\"\>(.*)\</ ){
 			die "Data format \"mm/dd/yyy\" expected for this cell."
 				unless $data =~ /^\d\d\/\d\d\/\d\d\d\d$/;
 			my $epoch = _ss_date_to_num($data);
-			$line =~ s/(\<gnm\:Cell Row\=\"$row\" Col\=\"$gnu_column\" ValueType\=\"\d+\"\ ValueFormat\=\"m\/d\/yyyy\"\>)(.*)(\<\/gnm\:Cell\>)/$1$epoch$3/;
-		} elsif( $line =~ s/(\<gnm\:Cell Row\=\"$row\" Col\=\"$gnu_column\" ValueType\=\"\d+\"\ ValueFormat\=\"\S+\"\>)(.*)(\<\/gnm\:Cell\>)/$1$data$3/){
-			print "Conditional\n";
-		}elsif( $line =~ s/(\<gnm\:Cell Row\=\"$row\" Col\=\"$gnu_column\" ValueType\=\"\d+\"\>)(.+)(\<\/gnm\:Cell\>)/$1$data$3/){
-			print "nonconditional\n";
+			$line =~ s/Row..$row. Col..$gnu_column.+\"m\/d\/yyyy\"\>(.*)\</$1$epoch$endcell/; 
+		}elsif( $line =~ s/Row..$row. Col..$gnu_column.+ValueFormat..\S+..(.*)\</$1$data$endcell/ ){
+			#print "Writing conditional cell.\n";
+		}elsif( $line =~ s/Row..$row. Col..$gnu_column.+ValueType..\d+..(.+)\</$1$data$endcell/ ){
+			#print "Writing nonconditional cell.\n";
 		}elsif( $line =~ /\<\/gnm\:Cells\>/ ){
 			# Did not find a line to change (cell is empty): create line at the end of gnm:Cell block.
-			#print $fhout "<gnm:Cell Row=\"$row\" Col=\"$gnu_column\" ValueType=\"60\">$data</gnm:Cell>\n";
-			#print $_ "<gnm:Cell Row=\"$row\" Col=\"$gnu_column\" ValueType=\"60\">$data</gnm:Cell>\n";
 			my $num = $i - 1;
-			#my $line = "<gnm:Cell Row=\"$row\" Col=\"$gnu_column\" ValueType=\"60    \">$data</gnm:    Cell>":
 			print "splicing line $num\n";
 			splice @temp_file, $num, 0, "<gnm:Cell Row=\"$row\" Col=\"$gnu_column\" ValueType=\"60\">$data</gnm:Cell>";
 			$i++;
-			#splice @temp_file, $num, 0, $line;
 		}
-		# Print line to outfile after changes have been made.
-#		print $fhout $_;
 	}
-	#close $fh;
-	#close $fhout;
-	#system("rm", $temp_file) == 0 or die "System call failed: $?";
-	#system("mv", "$temp_file.out", $temp_file) == 0 or die "System call failed: $?";
-	
 }
 
 sub _ss_num_to_date {
@@ -181,26 +160,4 @@ sub _ss_date_to_num {
     my ($month, $day, $year) = ($1, $2, $3);
     return Delta_Days($ss_epoch_year,$ss_epoch_month,$ss_epoch_day,$year,$month,$day);
 }
-
-#Leaving this unneeded sub for reference for now.
-##sub openfile {
-#	my $self = shift;
-#	my $gnumeric_ss = $self->{file};
-#
-#	#Use regex so code is applicable to any filename.
-#	$gnumeric_ss =~ /(.+)\.gnumeric$/;
-#	my $ss = $1;
-#	my $gz_ss = "$1.gz";
-#
-#	#Convert file to type that is useable by perl.
-#	rename $gnumeric_ss, $gz_ss;
-#	system("gunzip", $gz_ss) == 0 or die "System call failed: $?";
-#	system("cp", $ss, "copyof_sample_spreadsheet");
-#
-#	#cleanup
-#	system("gzip", $ss) == 0 or die "System call failed: $?";
-#	system("mv", $gz_ss, $gnumeric_ss) == 0 or die "System call failed: $?";
-#	#'copyof_spreadsheet' file not cleaned up for now for testing.
-#}
-
 1;
